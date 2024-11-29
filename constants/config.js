88f7e1dc78ab1f77a3c49f80bd776cb04e47916a
Sahil Gurnani <gurnanisahil87@gmail.com>
2024-11-29 16:48:01 +0530

    cors 13

--- constants/config.js
+++ constants/config.js
@@ -1,5 +1,5 @@
 export const corsOptions = {
-  origin: [process.env.CLIENT_URL, "http://localhost:5173"],
+  origin: process.env.CLIENT_URL,
   credentials: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
